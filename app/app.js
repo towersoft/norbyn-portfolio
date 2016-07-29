a91a4b30e3043e6b195bfce708260f0d2f27b872
Norbyn Leyva <norbyn.leyva@gases.co.cu>
2016-07-28 23:17:44 -0400

    test custom button directive

--- app/app.js
+++ app/app.js
@@ -2,16 +2,17 @@
 
 // Declare app level module which depends on views, and components
 angular.module('portfolio', [
-  'ngRoute',
-  'portfolio.home',
-  'portfolio.projects',
-  'portfolio.skills',
-  'portfolio.contact',
-  'portfolio.menu',
-  'portfolio.version'
+    'ngRoute',
+    'portfolio.home',
+    'portfolio.projects',
+    'portfolio.skills',
+    'portfolio.contact',
+    'portfolio.menu',
+    'portfolio.version',
+    'portfolio.button'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
+    config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+        $locationProvider.hashPrefix('!');
 
-  $routeProvider.otherwise({redirectTo: '/home'});
-}]);
+        $routeProvider.otherwise({redirectTo: '/home'});
+    }]);
--- app/modules/skills/skills.js
+++ app/modules/skills/skills.js
@@ -5,10 +5,15 @@ angular.module('portfolio.skills', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/skills', {
             templateUrl: 'modules/skills/skills.html',
-            controller: 'SkillsCtrl'
+            controller: 'SkillsCtrl',
+            controllerAs: 'vm'
         });
     }])
 
     .controller('SkillsCtrl', [function () {
-
+        var vm = this;
+        vm.something = "New skill";
+        vm.addSkill = function () {
+            alert("adding...");
+        }
     }]);
